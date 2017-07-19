4c87c64875dd3b6bb1820e62f8c6eccec9404795
luisvelazquex <lvelazquez@tvpage.com>
2017-07-19 15:19:56 -0700

    ready to start working

--- static/src/inline.jsx
+++ static/src/inline.jsx
@@ -1,1 +1,8 @@
- 
+var React = require('react');
+var ReactDOM = require('react-dom');
+class Inline extends React.Component{
+  render(){
+    return (<div>Widget {this.props.name}</div>);
+  }
+}
+ReactDOM.render(<Inline name="Inline"/>,document.getElementById("inline"));
